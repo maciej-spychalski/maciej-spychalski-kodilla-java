c28b0f5987039042d9049a41f1b0ec30f51edd82
trevize77 <maciej.spychalski@gmail.com>
2020-04-10 18:34:19 +0200

    First Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,35 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        int reslutOfAddition = calculator.add(10,5);
+        int reslutOfSubstraction = calculator.substract(10,5);
+
+        if (reslutOfAddition == 10 + 5) {
+            System.out.println("Test of additon: OK");
+        } else {
+            System.out.println("Test of additon: Error!");
+        }
+
+        if (reslutOfSubstraction == 10 - 5) {
+            System.out.println("Test of substraction: OK");
+        } else {
+            System.out.println("Test of substraction: Error!");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    public int add(int num1, int num2) {
+        int sum = num1 + num2;
+        return sum;
+    }
+
+    public int substract(int num1, int num2) {
+        int sum = num1 - num2;
+        return sum;
+    }
+}
