8d9990b5953b55c5cc7c77378a0c96e30bab4682
trevize77 <maciej.spychalski@gmail.com>
2020-04-12 18:22:01 +0200

    First Commit

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -7,32 +7,36 @@ import java.util.ArrayList;
 public class CollectionTestSuite {
 
     @Before
-    public void before(){
+    public void before() {
         System.out.println("Test Case: begin");
     }
+
     @After
-    public void after(){
+    public void after() {
         System.out.println("Test Case: end");
     }
+
     @BeforeClass
     public static void beforeClass() {
         System.out.println("Test Suite: begin");
     }
+
     @AfterClass
     public static void afterClass() {
         System.out.println("Test Suite: end");
     }
+
     @Test
     public void testOddNumbersExterminatorEmptyList() {
         //Given
         ArrayList<Integer> allNumber = new ArrayList<>();
         //When
-        ArrayList<Integer> rightNumber = new ArrayList<>();
-        rightNumber.addAll(OddNumbersExterminator.exterminate(allNumber));
+        ArrayList<Integer> rightNumber = OddNumbersExterminator.exterminate(allNumber);
         System.out.println("Testing empty list");
         //Then
         Assert.assertTrue(rightNumber.isEmpty());
     }
+
     @Test
     public void testOddNumbersExterminatorNormalList() {
         //Given
@@ -41,16 +45,13 @@ public class CollectionTestSuite {
             allNumber.add(i);
         }
         //When
-        ArrayList<Integer> rightNumber = new ArrayList<>();
-        rightNumber.addAll(OddNumbersExterminator.exterminate(allNumber));
-        boolean isRightNumber = true;
-        for (int i = 0; i < rightNumber.size(); i++) {
-            if (rightNumber.get(i) % 2 != 0) {
-                isRightNumber = false;
-            }
-        }
+        ArrayList<Integer> rightNumber = OddNumbersExterminator.exterminate(allNumber);
         System.out.println("Testing OddNumberExterminator method");
+        boolean isProperElement;
         //Then
-        Assert.assertTrue(isRightNumber);
+        for (int i = 0; i < rightNumber.size(); i++) {
+            isProperElement = rightNumber.get(i) % 2 == 0;
+            Assert.assertTrue(isProperElement);
+        }
     }
 }
