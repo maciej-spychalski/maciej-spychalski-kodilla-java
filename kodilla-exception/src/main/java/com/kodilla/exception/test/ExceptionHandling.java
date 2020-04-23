29ff48a52741bed83845887f0a4f4bcd3668fef8
trevize77 <maciej.spychalski@gmail.com>
2020-04-23 09:36:57 +0200

    First Commit

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -3,10 +3,12 @@ package com.kodilla.exception.test;
 public class ExceptionHandling {
     SecondChallenge secondChallenge = new SecondChallenge();
 
-    try {
-        String s = secondChallenge.probablyIWillThrowException(1.0, 1.5);
-        System.out.println(s);
-    } catch (Exception e) {
-        System.out.println("x should be in range <1,2) and y cannot be equal 1.5");
+    public void handleException() {
+        try {
+            String s = secondChallenge.probablyIWillThrowException(1.0, 1.5);
+            System.out.println(s);
+        } catch (Exception e) {
+            System.out.println("x should be in range <1,2) and y cannot be equal 1.5");
+        }
     }
 }
