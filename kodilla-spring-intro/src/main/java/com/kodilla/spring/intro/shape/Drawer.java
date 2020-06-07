befa40bfb23dc1a48b030ac62350b9647dfc1b83
trevize77 <maciej.spychalski@gmail.com>
2020-06-07 19:04:13 +0200

    First Commit

--- kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/shape/Drawer.java
+++ kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/shape/Drawer.java
@@ -1,11 +1,13 @@
 package com.kodilla.spring.intro.shape;
 
 public class Drawer {
-    public void doDrawing() {
-        Circle circle = new Circle();
-        Triangle triangle = new Triangle();
+    final Shape shape;
 
-        circle.draw();
-        triangle.draw();
+    public Drawer(final Shape shape) {
+        this.shape = shape;
+    }
+
+    public String doDrawing() {
+        return shape.draw();
     }
 }
