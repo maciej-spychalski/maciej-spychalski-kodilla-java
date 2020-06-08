36d71a82fea26445a5de5ff3e70174ea5e05b057
trevize77 <maciej.spychalski@gmail.com>
2020-06-08 17:01:13 +0200

    First Commit

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -0,0 +1,37 @@
+package com.kodilla.spring.portfolio;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BoardConfig {
+    @Autowired
+    TaskList toDoList;
+    @Autowired
+    TaskList inProgressList;
+    @Autowired
+    TaskList doneList;
+
+    @Bean
+    public Board getBoard() {
+        return new Board(toDoList, inProgressList, doneList);
+    }
+
+    @Bean(name = "toDoList")
+    public TaskList getToDoList() {
+        return new TaskList();
+    }
+
+    @Bean(name = "inProgressList")
+    public TaskList getInProgressList() {
+        return new TaskList();
+    }
+
+    @Bean(name = "doneList")
+    public TaskList getDoneList() {
+        return new TaskList();
+    }
+}
+
