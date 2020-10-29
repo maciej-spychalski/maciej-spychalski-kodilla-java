ac000ad06cf90d9a17330f19d1fa86f3ed58312f
trevize77 <maciej.spychalski@gmail.com>
2020-10-29 13:08:46 +0100

    Task30.2

---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/ebay/EBayTestingApp.java
@@ -0,0 +1,23 @@
+package com.kodilla.testing2.ebay;
+
+import com.kodilla.testing2.google.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class EBayTestingApp {
+//    public static final String SEARCH_CLASS = "input#gh-ac.gh-tb.ui-autocomplete-input";
+//    public static final String SEARCH_CLASS = ".gh-tb.ui-autocomplete-input";
+    public static final String SEARCH_NAME = "_nkw";
+
+
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.FIREFOX);
+        driver.get("https://www.ebay.com/");
+
+//        WebElement searchField = driver.findElement(By.cssSelector(SEARCH_CLASS));
+        WebElement searchField = driver.findElement(By.name(SEARCH_NAME));
+        searchField.sendKeys("Laptop");
+        searchField.submit();
+    }
+}
---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
@@ -0,0 +1,22 @@
+package com.kodilla.testing2.google;
+
+import com.kodilla.testing2.google.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class GoogleTestingApp {
+//    public static final String SEARCHFIELD = "1st-ib";
+    public static final String SEARCHCLASS = ".gLFyf.gsfi";
+//    public static final String SEARCHCLASS = "input.gLFyf.gsfi";
+
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.FIREFOX);
+        driver.get("http://www.google.com");
+
+//        WebElement searchField = driver.findElement(By.id(SEARCHFIELD));
+        WebElement searchField = driver.findElement(By.cssSelector(SEARCHCLASS));
+        searchField.sendKeys("Kodilla");
+        searchField.submit();
+    }
+}
